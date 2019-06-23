f161ef6309c564b20a0dc21d0bbaf518eedc9df0
Ashish Yadav <ashudva@gmail.com>
2019-06-23 11:48:28 +0530

    Add solution : debugging

--- project0/debug.py
+++ project0/debug.py
@@ -1,13 +1,17 @@
-def get_sum_metrics(predictions, metrics=[]):
+def get_sum_metrics(predictions, metrics = None):
+    """ Create a new object each time the function is called, by using a default arg to signal that no argument was provided
+        Do not use metrics = [] as it does not Create a new list each time the function is called instead it append to the list
+        generated at the first call """
+    if metrics is None:
+        metrics = []
     for i in range(3):
-        metrics.append(lambda x: x + i)
-
+        """ this bug is due the late binding closure """
+        metrics.append(lambda x, i=i: x + i)
     sum_metrics = 0
     for metric in metrics:
         sum_metrics += metric(predictions)
-
+    metrics = []
     return sum_metrics
-
 
 def main():
     print(get_sum_metrics(0))  # Should be (0 + 0) + (0 + 1) + (0 + 2) = 3
