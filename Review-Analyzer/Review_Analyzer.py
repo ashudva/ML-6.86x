bc7d9653b78a1f76e9d7b0450e953379e8798f22
Ashish Yadav <ashudva@gmail.com>
2019-07-01 19:37:24 +0530

    Single step perceptron

--- Review-Analyzer/Review_Analyzer.py
+++ Review-Analyzer/Review_Analyzer.py
@@ -50,24 +50,14 @@ def perceptron_single_step_update(
         label,
         current_theta,
         current_theta_0):
-    """
-    Properly updates the classification parameter, theta and theta_0, on a
-    single step of the perceptron algorithm.
-
-    Args:
-        feature_vector - A numpy array describing a single data point.
-        label - The correct classification of the feature vector.
-        current_theta - The current theta being used by the perceptron
-            algorithm before this update.
-        current_theta_0 - The current theta_0 being used by the perceptron
-            algorithm before this update.
-
-    Returns: A tuple where the first element is a numpy array with the value of
-    theta after the current update has completed and the second element is a
-    real valued number with the value of theta_0 after the current updated has
-    completed.
-    """
-    # Your code here
+    
+    for i in range(3):
+        new_theta = current_theta
+        new_theta_0 = current_theta_0
+        if label * (np.dot(current_theta,feature_vector) + current_theta_0) <= 0:
+            new_theta = new_theta + label*feature_vector
+            new_theta_0 = new_theta_0 + label
+    return new_theta,new_theta_0
     raise NotImplementedError
 #pragma: coderesponse end
 
