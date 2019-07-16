40ec2dc401a5edb5420d75eb8bbb30ecd3366cd6
Ashish Yadav <ashudva@gmail.com>
2019-07-16 21:46:50 +0530

    Bug fix : run_linear_regression_on_MINST

--- Digit_Recognition/OneDigit/main.py
+++ Digit_Recognition/OneDigit/main.py
@@ -34,8 +34,8 @@ def run_linear_regression_on_MNIST(lambda_factor=1):
     train_x, train_y, test_x, test_y = get_MNIST_data()
     train_x_bias = np.hstack([np.ones([train_x.shape[0], 1]), train_x])
     test_x_bias = np.hstack([np.ones([test_x.shape[0], 1]), test_x])
-    theta = closed_form(train_x, train_y, lambda_factor)
-    test_error = compute_test_error_linear(test_x, test_y, theta)
+    theta = closed_form(train_x_bias, train_y, lambda_factor)
+    test_error = compute_test_error_linear(test_x_bias, test_y, theta)
     return test_error
 
 
