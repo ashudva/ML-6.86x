578f72ae163b63af97dc7980da6ca0a6d0260182
Ashish Yadav <ashudva@gmail.com>
2019-07-22 21:19:38 +0530

    Perceptron single update and full

--- Review-Analyzer/Review_Analyzer.py
+++ Review-Analyzer/Review_Analyzer.py
@@ -33,27 +33,64 @@ def perceptron_single_step_update(
         label,
         current_theta,
         current_theta_0):
+    """
+    Properly updates the classification parameter, theta and theta_0, on a
+    single step of the perceptron algorithm.
 
-    for i in range(3):
-        new_theta = current_theta
-        new_theta_0 = current_theta_0
-        if label * (np.dot(current_theta,feature_vector) + current_theta_0) <= 0:
-            new_theta = new_theta + label*feature_vector
-            new_theta_0 = new_theta_0 + label
-    return new_theta,new_theta_0
+    Args:
+        feature_vector - A numpy array describing a single data point.
+        label - The correct classification of the feature vector.
+        current_theta - The current theta being used by the perceptron
+            algorithm before this update.
+        current_theta_0 - The current theta_0 being used by the perceptron
+            algorithm before this update.
+
+    Returns: A tuple where the first element is a numpy array with the value of
+    theta after the current update has completed and the second element is a
+    real valued number with the value of theta_0 after the current updated has
+    completed.
+
+    We need to make sure that if the output of the perceptron is  0 , the weights are still updated, hence the check for large inequality.
+    In fact, because of numerical instabilities, it is preferable to identify  0  with a small range  [−ε,ε] .
+
+    """
+    if label * (np.dot(current_theta, feature_vector) + current_theta_0) <= 1e-7:
+        current_theta += label * feature_vector
+        current_theta_0 += label
+    return (current_theta, current_theta_0)
 
 
 def perceptron(feature_matrix, labels, T):
+    """
+    Runs the full perceptron algorithm on a given set of data. Runs T
+    iterations through the data set, there is no need to worry about
+    stopping early.
 
-    new_theta = np.zeros((feature_matrix.shape[1],))
-    new_theta_0 = 0
+    NOTE: Please use the previously implemented functions when applicable.
+    Do not copy paste code from previous parts.
+
+    Args:
+        feature_matrix -  A numpy matrix describing the given data. Each row
+            represents a single data point.
+        labels - A numpy array where the kth element of the array is the
+            correct classification of the kth row of the feature matrix.
+        T - An integer indicating how many times the perceptron algorithm
+            should iterate through the feature matrix.
+
+    Returns: A tuple where the first element is a numpy array with the value of
+    theta, the linear classification parameter, after T iterations through the
+    feature matrix and the second element is a real number with the value of
+    theta_0, the offset classification parameter, after T iterations through
+    the feature matrix.
+    """
+    (nsamples, nfeatures) = feature_matrix.shape
+    theta = np.zeros(nfeatures)
+    theta_0 = 0.0
     for t in range(T):
-        for i in get_order(feature_matrix.shape[0]):
-            updated_data = perceptron_single_step_update(feature_matrix[i],labels[i],new_theta,new_theta_0)
-            new_theta = updated_data[0]
-            new_theta_0 = updated_data[1]
-            pass
-    return new_theta,new_theta_0
+        for i in get_order(nsamples):
+            theta, theta_0 = perceptron_single_step_update(
+                feature_matrix[i], labels[i], theta, theta_0)
+    return (theta, theta_0)
 
 
 def average_perceptron(feature_matrix, labels, T):
