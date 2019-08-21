931ff72734db31e25e0f64264dd01f79d9e37dd8
Ashish Yadav <ashudva@gmail.com>
2019-08-22 01:01:16 +0530

    K & seed tuning , EM implementation
    Implementation of simple version of EM algorithm and Parameters tuning
    K = [1,2,3,4] and seed = [0,1,2,3,4]

--- Netflix/main.py
+++ Netflix/main.py
@@ -4,6 +4,14 @@ import common
 import naive_em
 import em
 
+# Parameters tuning K and seed
 X = np.loadtxt("toy_data.txt")
-
-# TODO: Your code here
+costs = np.zeros((4,5))
+for K in range(1,5):
+    for seed in range(5):
+        mixture, post = common.init(X, K, seed)
+        mixture, post, costs[K-1,seed] = kmeans.run(X, mixture, post)
+        # uncomment below code for plots
+        # common.plot(X, mixture, post, f"mixtures when K = {K}, seed = {seed}")
+# print minimum cost for different K = [1,2,3,4]
+print(np.min(costs, axis=1))
--- Netflix/naive_em.py
+++ Netflix/naive_em.py
@@ -17,7 +17,38 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
             for all components for all examples
         float: log-likelihood of the assignment
     """
-    raise NotImplementedError
+    n,d = X.shape
+    mu, var, pi = mixture
+    K = mu.shape[0]
+
+    # Posterior probabilities & Normal matrix
+    post = np.zeros((n, K), dtype=np.float64)
+
+    # Compute normal dist. matrix: (N, K)
+    pre_exp = (2*np.pi*var)**(d/2)
+
+    # Using single loop to complete Normal matrix: faster than broadcasting in 3D
+    for i in range(n):
+        # Compute difference: will be (K,d) for each n
+        dist = X[i, :] - mu
+        # Norm: will be (K,) for each n
+        norm = np.sum(dist**2, axis=1)
+        # Exponent term of normal
+        post[i, :] = np.exp(-norm/(2*var))
+
+    # Normal matrix: (n, K)
+    post = post/pre_exp
+
+    numerator = post*pi
+    # vector p(x;theta)
+    denominator = np.sum(numerator, axis=1).reshape(-1, 1)
+
+    # This is the matrix of posterior probs p(j|i)
+    post = numerator/denominator
+
+    log_lh = np.sum(np.log(denominator), axis=0).item()    # Log-likelihood
+
+    return post, log_lh
 
 
 def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
@@ -32,7 +63,24 @@ def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
     Returns:
         GaussianMixture: the new gaussian mixture
     """
-    raise NotImplementedError
+    n,d = X.shape
+    K = post.shape[1]
+
+    nj = np.sum(post, axis=0)   # shape is (K, )
+
+    pi = nj/n   # Cluster probs; shape is (K, )
+
+    mu = (post.T @ X)/nj.reshape(-1,1)  # Revised means; shape is (K,d)
+
+    norms = np.zeros((n, K), dtype=np.float64)   # Matrix to hold all the norms: (n,K)
+
+    for i in range(n):
+        dist = X[i,:] - mu
+        norms[i,:] = np.sum(dist**2, axis=1)
+
+    var = np.sum(post*norms, axis=0)/(nj*d)     # Revised variance; shape is (K, )
+
+    return GaussianMixture(mu, var, pi)
 
 
 def run(X: np.ndarray, mixture: GaussianMixture,
@@ -50,4 +98,18 @@ def run(X: np.ndarray, mixture: GaussianMixture,
             for all components for all examples
         float: log-likelihood of the current assignment
     """
-    raise NotImplementedError
+    old_log_lh = None
+    new_log_lh = None  # Keep track of log likelihood to check convergence
+
+    # Start the main loop
+    while old_log_lh is None or (new_log_lh - old_log_lh > 1e-6*np.abs(new_log_lh)):
+
+        old_log_lh = new_log_lh
+
+        # E-step
+        post, new_log_lh = estep(X, mixture)
+
+        # M-step
+        mixture = mstep(X, post)
+
+    return mixture, post, new_log_lh
