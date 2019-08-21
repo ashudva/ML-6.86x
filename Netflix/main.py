48e8114d68107c5fa55bc616324d66ba8c4fefe8
Ashish Yadav <ashudva@gmail.com>
2019-08-22 01:44:44 +0530

    Closes #38
    1. In the case K=1, the mixture parameters and point assignments are the
     same for both methods
    2. In the case K=2, both methods have similar parameters and point
    assignments
    3. In the case K=3, the EM solution accounts for point density
    better than K-means
    4. In the case K=4, the k-means solution equally spaces the clusters to
    minimize distortion cost

--- Netflix/main.py
+++ Netflix/main.py
@@ -6,12 +6,29 @@ import em
 
 # Parameters tuning K and seed
 X = np.loadtxt("toy_data.txt")
+
+#---------------------------
+#  Part 1 : Using kmeans
+# --------------------------
+
 costs = np.zeros((4,5))
 for K in range(1,5):
     for seed in range(5):
         mixture, post = common.init(X, K, seed)
         mixture, post, costs[K-1,seed] = kmeans.run(X, mixture, post)
-        # uncomment below code for plots
-        # common.plot(X, mixture, post, f"mixtures when K = {K}, seed = {seed}")
+        common.plot(X, mixture, post, f"mixtures when K = {K}, seed = {seed}")
 # print minimum cost for different K = [1,2,3,4]
 print(np.min(costs, axis=1))
+
+#---------------------------
+#  Part 2 : Using naive_em
+# --------------------------
+
+log_lh = np.zeros((4, 5))
+for K in range(1, 5):
+    for seed in range(5):
+        mixture, post = common.init(X, K, seed)
+        mixture, post, log_lh[K-1, seed] = naive_em.run(X, mixture, post)
+        common.plot(X, mixture, post, f"mixtures when K = {K}, seed = {seed}")
+# print maximum log likelihood for different K = [1,2,3,4]
+print(np.max(log_lh, axis=1))
